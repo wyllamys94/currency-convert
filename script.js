728b907563d317b8329e4909ac006391241a3bc7
wyllamys94 <wyllamysynwram@gmail.com>
2024-02-25 17:07:18 -0300

    projeto responsivo

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector(".formulario1")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+function cliqueiNoBotao() {
+
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+    
+}
+
+function esconderForm(){
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
+
+}
